a3396db22058de45a08edd1e7de91462be0f81ed
Benoit Chazoule <benoit.chazoule.pro@gmail.com>
2018-02-05 12:33:34 +0100

    Ajout de la possibilité d'enregistrer les prix d'un lieu en même temps que la création de ce lieu

---
+++ src/AppBundle/Form/Validator/Constraint/PriceTypeUnique.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Form\Validator\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class PriceTypeUnique extends Constraint
+{
+    public $message = 'A place cannot contain prices with same type';
+}
+
